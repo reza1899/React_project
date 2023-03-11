5895487e086ce26d6e83197312e8f6393977edfe
reza1899 <reza.pk.lu9@gmail.com>
2023-03-10 16:13:58 -0800

    view contact in updated

--- my-app/src/components/contact/ViewContact.jsx
+++ my-app/src/components/contact/ViewContact.jsx
@@ -1,8 +1,9 @@
 import {useEffect, useState} from "react";
 import {getContact,getGroupId} from "../../services/services";
-import {useParams} from "react-router-dom";
+import {useNavigate, useParams} from "react-router-dom";
 import {NARANJI, ROS} from "../../helpers/Colors";
 const ViewContact = () => {
+    const navigate = useNavigate()
    const [state , setstate] = useState({
        contact : {},
        group : {}
@@ -30,33 +31,35 @@ const ViewContact = () => {
     const {contact,group} = state
     return (
         <>
-            {Object.keys(contact).length > 0 && (<div className="col-md-6">
-                <div style={{backgroundColor:ROS}} className="container card my-2">
-                    <div className="card-body">
-                        <div className="row align-items-center d-flex justify-content-around">
-                            <div className="col-md-4 col-sm-4 ">
-                                <img className='rounded-3 img-fluid' src={contact.image} alt="user" />
-                            </div>
-                            <div className="col-md-7 col-sm-7">
-                                <div>
-                                    <ul className="list-group">
-                                        <li style={{backgroundColor:NARANJI}} className="list-group-item">نام و نام خانوداگی:{" "}<span className="fw-bold">{contact.fullName}</span></li>
-                                        <li style={{backgroundColor:NARANJI}} className="list-group-item">شماره موبایل: {" "} <span className="fw-bold">{contact.mobile}</span></li>
-                                        <li style={{backgroundColor:NARANJI}} className="list-group-item">آدرس ایمیل : {" "} <span className="fw-bold">{contact.email}</span></li>
-                                        <li style={{backgroundColor:NARANJI}} className="list-group-item">دسته:{" "}<span className="fw-bold">{group.name}</span></li>
+            {Object.keys(contact).length > 0 && (
+                <>
+                    <div className="col-md-6 d-block">
+                        <div style={{backgroundColor:ROS}} className="container card my-2">
+                            <div className="card-body">
+                                <div className="row align-items-center d-flex justify-content-around">
+                                    <div className="col-md-4 col-sm-4 ">
+                                        <img className='rounded-3 img-fluid' src={contact.image} alt="user" />
+                                    </div>
+                                    <div className="col-md-7 col-sm-7">
+                                        <div>
+                                            <ul className="list-group">
+                                                <li style={{backgroundColor:NARANJI}} className="list-group-item">نام و نام خانوداگی:{" "}<span className="fw-bold">{contact.fullName}</span></li>
+                                                <li style={{backgroundColor:NARANJI}} className="list-group-item">شماره موبایل: {" "} <span className="fw-bold">{contact.mobile}</span></li>
+                                                <li style={{backgroundColor:NARANJI}} className="list-group-item">آدرس ایمیل : {" "} <span className="fw-bold">{contact.email}</span></li>
+                                                <li style={{backgroundColor:NARANJI}} className="list-group-item">دسته:{" "}<span className="fw-bold">{group.name}</span></li>
 
-                                    </ul>
+                                            </ul>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
-                            <div className="col-md-1 col-sm-1 d-flex flex-column align-items-center">
-                                <button className="btn bg-warning"><i className="fa fa-eye"/></button>
-                                <button className="btn bg-info my-2"><i className="fa fa-pen"/></button>
-                                <button className="btn bg-danger"><i className="fa fa-trash"/></button>
-                            </div>
+                            <button className="btn btn-primary m-4" onClick={() => {
+                                navigate ("/contacts")
+                            }}>بازگشت به صفحه اصلی</button>
                         </div>
                     </div>
-                </div>
-            </div>)}
+                </>
+            )}
         </>
     )
 }
