119d61415f50b431337b2caa2a61771a15b2b39a
reza1899 <reza.pk.lu9@gmail.com>
2023-05-06 16:03:57 -0700

    Update Acordian.jsx

--- my-app/src/components/Hooks/Acordian.jsx
+++ my-app/src/components/Hooks/Acordian.jsx
@@ -194,7 +194,7 @@ console.log(users)
              </div>
             </div>
           </div>
-        </div> 
+        </div>  
         {/*<div className="accordion-item">*/}
         {/*  <h2 className="accordion-header" id="headingSix">*/}
         {/*    <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSix" aria-expanded="false" aria-controls="collapseSix">*/}
