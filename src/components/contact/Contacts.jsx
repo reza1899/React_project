9f6acd946dff5936ebd60789b57720144a03da3c
reza1899 <reza.pk.lu9@gmail.com>
2023-05-26 16:08:17 -0700

    add note for user

--- src/components/contact/Contacts.jsx
+++ src/components/contact/Contacts.jsx
@@ -10,6 +10,10 @@ const Contacts = ({newContacts , loading}) => {
                    <div className="text-center">
                      <h3 style={{color:NARANJI}} className="">...مخاطب یافت نشد</h3>
                     <img className="w-25 rounded-3" src={require("../../assets/R.gif")} alt="a gif" />
+                     <h1 style={{color:NARANJI}}>اگر میخواهید وب را مشهاده نمایید لطفا به ادرس گیت من که در پایین درج شده بروید و پروژه رو کلون کنید و ران کنید 
+                         <br />
+                         reza1899
+                        </h1>
                    </div>
                 ) : (
                    <div className="row m-0">
