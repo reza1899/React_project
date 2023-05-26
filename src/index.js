854a9305f454ccfe0ed58f0bae76242c633f59d4
reza1899 <reza.pk.lu9@gmail.com>
2023-05-26 09:49:18 -0700

    app is online on github now

--- src/index.js
+++ src/index.js
@@ -14,9 +14,9 @@ import {BrowserRouter} from "react-router-dom"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+  <BrowserRouter basename='React_project'>
        <App/>
-    </BrowserRouter>
+  </BrowserRouter>
   </React.StrictMode>
 );
 reportWebVitals();
