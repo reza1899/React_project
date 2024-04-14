b66589b05a71b01dd80b643bcff14e5db258d4a0
reza1899 <reza.pk.lu9@gmail.com>
2024-04-14 03:57:34 -0700

    react helmet library is done

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import axios from "axios";
 import contextApi from "./context/contextApi" 
 import {Routes,Route,Navigate} from "react-router-dom"
 import {ToastContainer, toast} from "react-toastify"
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 import {
     Navbar,
     Contacts,
@@ -55,19 +56,26 @@ return(
     setNewContacts,
     refresh,
   }}>
-    <div className="App">
-        <ToastContainer rtl={true} position = {"top-right"} theme={"colored"}/>
-    <Navbar />
-      <Routes>
-          <Route path="/" element={<Navigate to="/contacts"/>}/>
-          <Route path = "/contacts" element={<Contacts newContacts={newContacts} loading={loading}/>}/>
-          <Route path ="/add" element={<AddContact loading ={loading} updatePage={updatePage} setUpdatePage={setUpdatePage}/>}/>
-          <Route path ="/edit/:contactId" element={<Edit loading={loading} refresh ={refresh}/>}/>
-          <Route path = "/contacts/:contactId" element={<ViewContact  loading={loading}/>}/>
-          <Route path = "/acordian" element={<Acordian/>} />
-          <Route path ="/seson12" element ={<Seson12/>}></Route>
-      </Routes>
-  </div>
+    <HelmetProvider>
+        <div className="App">
+            <Helmet>
+                <title>
+                    دوره جامع ری اکت
+                </title>
+            </Helmet>
+            <ToastContainer rtl={true} position = {"top-right"} theme={"colored"}/>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Navigate to="/contacts"/>}/>
+                <Route path = "/contacts" element={<Contacts newContacts={newContacts} loading={loading}/>}/>
+                <Route path ="/add" element={<AddContact loading ={loading} updatePage={updatePage} setUpdatePage={setUpdatePage}/>}/>
+                <Route path ="/edit/:contactId" element={<Edit loading={loading} refresh ={refresh}/>}/>
+                <Route path = "/contacts/:contactId" element={<ViewContact  loading={loading}/>}/>
+                <Route path = "/acordian" element={<Acordian/>} />
+                <Route path ="/seson12" element ={<Seson12/>}></Route>
+            </Routes>
+        </div>
+    </HelmetProvider>
   </contextApi.Provider>
 )
 }
